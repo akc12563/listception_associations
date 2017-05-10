ba7740f1ff7f3cd3cf5aa0c7c5b0da96e1c6de0d
Amy Collins <amy.collins@user.noreply.github.com>
2017-05-10 18:45:22 -0500

    cards and lists with description

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,4 @@
 class Card < ApplicationRecord
 belongs_to(:list, :class_name=>"List", :foreign_key=>"list_id")
+has_many(:tags, :class_name=>"Tagging",:foreign_key=>"card_id")
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
   validates :name, :uniqueness => :true
+  belongs_to(:card, :class_name=>"Card", :foreign_key=>"card_id")
 end
